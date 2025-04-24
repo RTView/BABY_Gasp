143bc1b59c1a6eb70651d89cb43bdbecc1d3bc55
Saul <saul.clemente@gmail.com>
2025-04-24 14:38:47 +0200

    Sfondo bianco 2 tentativo

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,10 @@ const engine = new BABYLON.Engine(canvas, true);
 
 const createScene = async () => {
     const scene = new BABYLON.Scene(engine);
+
+    scene.createDefaultEnvironment({ createSkybox: false, enableGroundShadow: false });
     scene.clearColor = new BABYLON.Color4(1, 1, 1, 1);
+
     const camera = new BABYLON.ArcRotateCamera("camera", Math.PI / 2, Math.PI / 2.5, 5, BABYLON.Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), scene);
